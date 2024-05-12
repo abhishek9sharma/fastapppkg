a336f7362b1d02ab408f06520a5e717cbf6ad863
abhishek9sharma <abhishek9sharma@users.noreply.github.com>
2024-05-12 03:38:01 +0000

    Refactor math operations functions and API endpoints.
    - Add docstrings explaining input and output for each operation.
    - Update root service and routers to utilize the new docstrings.
    - Enhance code readability and maintainability.

--- src/fastapppkg/mathops/ops.py
+++ src/fastapppkg/mathops/ops.py
@@ -1,14 +1,49 @@
 def addop(a, b):
+    """Returns the sum of two numbers.
+
+    Args:
+        a: The first number.
+        b: The second number.
+
+    Returns:
+        The sum of the two numbers.
+    """
     return a + b
 
 
 def subop(a, b):
+    """Subtracts two numbers.
+
+    Args:
+        a: The first number.
+        b: The second number.
+
+    Returns:
+        The result of subtracting b from a.
+    """
     return a - b
 
 
 def multop(a, b):
+    """Multiply two numbers.
+
+    Args:
+        a: The first number.
+        b: The second number.
+
+    Returns:
+        The result of multiplying the two numbers.
+    """
     return a * b
 
 
 def invertop(a):
+    """Return the inverse of the input number.
+
+    Args:
+        a: The input number for which inverse needs to be calculated.
+
+    Returns:
+        The inverse of the input number as a negative value.
+    """
     return -1
--- src/fastapppkg/restapi/routers/mathopsrouter.py
+++ src/fastapppkg/restapi/routers/mathopsrouter.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter
-
 from fastapppkg.mathops.ops import multop, subop
 from fastapppkg.schemas.mathops import MathOpsReq, MathOpsResp
 
@@ -8,11 +7,27 @@ router = APIRouter()
 
 @router.post("/sub")
 def sub(add_req: MathOpsReq):
+    """Performs subtraction operation.
+
+    Args:
+        add_req: An object of MathOpsReq class containing two numbers for subtraction.
+
+    Returns:
+        A dictionary containing the result of the subtraction operation.
+    """
     ans = subop(add_req.num_1, add_req.num_2)
     return {"ans": ans}
 
 
 @router.post("/mult")
 def sub(add_req: MathOpsReq):
+    """Performs multiplication operation.
+
+    Args:
+        add_req: An object containing two numbers for multiplication.
+
+    Returns:
+        A dictionary containing the result of the multiplication operation.
+    """
     ans = multop(add_req.num_1, add_req.num_2)
     return {"ans": ans}
